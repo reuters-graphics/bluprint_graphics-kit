09f1f8f015d0f1da7f8bc01021cf6bd5b4fd2855
Jon McClure <jon.r.mcclure@gmail.com>
2021-04-04 13:02:29 +0100

    Build for publish

--- pages/index.svelte
+++ pages/index.svelte
@@ -1,12 +1,16 @@
 <script>
 	import Page from '$lib/Page.svelte';
 	import '$lib/translations';
+	import { assets } from '$app/paths';
 
 	import { init } from 'svelte-i18n';
 	
 	init({ initialLocale: 'en' });
 </script>
 
+<svelte:head>
+	<meta property="og:image" content={`${assets}/${('images/shark.jpg').replace(/^\/+/, '')}`}>
+</svelte:head>
 
 <Page />
 
