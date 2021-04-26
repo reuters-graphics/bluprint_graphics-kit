062ada9fdc147b23550e62fa4b1d00edd89fe799
hobbes7878 <jon.r.mcclure@gmail.com>
2021-04-26 18:20:06 +0100

    load font faces outside global

--- pages/index.svelte
+++ pages/index.svelte
@@ -1,16 +1,23 @@
 <script>
-import { SEO, Sharer, Referrals, Nav, Footer, EmbedPreviewerLink } from 'reuters-components';
-import Page from '$lib/Page.svelte';
-import content from '$locales/en/content.json';
+  import {
+    SEO,
+    Sharer,
+    Referrals,
+    Nav,
+    Footer,
+    EmbedPreviewerLink,
+  } from 'reuters-components';
+  import Page from '$lib/Page.svelte';
+  import content from '$locales/en/content.json';
 </script>
 
-<SEO 
-	seoTitle={content.SEOTitle}
-	seoDescription={content.SEODescription}
-	shareTitle={content.ShareTitle}
-	shareDescription={content.ShareDescription}
-	shareImgPath='images/shark.jpg'
-	lang='en'
+<SEO
+  seoTitle="{content.SEOTitle}"
+  seoDescription="{content.SEODescription}"
+  shareTitle="{content.ShareTitle}"
+  shareDescription="{content.ShareDescription}"
+  shareImgPath="images/shark.jpg"
+  lang="en"
 />
 
 <Nav />
@@ -23,7 +30,8 @@ import content from '$locales/en/content.json';
 <EmbedPreviewerLink />
 
 <style lang="scss">
-:global {
-	@import "@reuters-graphics/style-theme-eisbaer/scss/main";
-}
+  @import '~@reuters-graphics/style-main/scss/fonts/font-faces';
+  :global {
+    @import '@reuters-graphics/style-theme-eisbaer/scss/main';
+  }
 </style>
