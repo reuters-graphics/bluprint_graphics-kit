572350ae5bed5afbae170d9db5f6f8d918e4cda3
hobbes7878 <jon.r.mcclure@gmail.com>
2021-06-04 11:40:35 +0100

    update referrals data source, bump vite, sveltekit and reuters theme

--- src/FutureGraphicsComponentsLib/Referrals/Referrals.svelte
+++ src/FutureGraphicsComponentsLib/Referrals/Referrals.svelte
@@ -1,29 +1,41 @@
 <script>
-import { onMount } from 'svelte';
-import Link from './Link.svelte';
+  import { onMount } from 'svelte';
+  import Link from './Link.svelte';
 
-let metadata;
+  let metadata;
 
-onMount(() => {
-  fetch('https://graphics.thomsonreuters.com/data/reuters-graphics/homepage/graphics.json')
-    .then(resp => resp.json())
-    .then((data) => {
-      metadata = data.slice(0, 4).map(({ url, image, title, description }) => ({ url, image, title, description }));
-    });
-});
-
+  onMount(() => {
+    fetch(
+      'https://graphics.thomsonreuters.com/data/reuters-graphics/homepage/latest.json'
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        metadata = data
+          .slice(0, 4)
+          .map(({ url, image, title, description }) => ({
+            url,
+            image,
+            title,
+            description,
+          }));
+      });
+  });
 </script>
+
 {#if metadata}
-<section class='referral-container'>
-  <figure>
-    <img src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/logo-color.svg" alt="" />
-  </figure>
-  <nav class='referral-rail row'>
-    {#each metadata as referral}
-      <Link {...referral} />
-    {/each}
-  </nav>
-</section>
+  <section class="referral-container">
+    <figure>
+      <img
+        src="https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-logo/svg/logo-color.svg"
+        alt=""
+      />
+    </figure>
+    <nav class="referral-rail row">
+      {#each metadata as referral}
+        <Link {...referral} />
+      {/each}
+    </nav>
+  </section>
 {/if}
 
 <style>
