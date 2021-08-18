0b53c3b339e3f4820881401dfffa97479039c934
hobbes7878 <jon.r.mcclure@gmail.com>
2021-08-18 22:13:17 +0100

    upgrade components lib

--- src/lib/Page.svelte
+++ src/lib/Page.svelte
@@ -4,17 +4,21 @@
     BodyText,
     Image,
     EndNotes,
+    Headline,
   } from '@reuters-graphics/graphics-svelte-components';
   import Chart from './Chart.svelte';
 </script>
 
 <article class="container-fluid">
-  <section class="headline">
-    <p class="section-title color-red">{content.Kicker}</p>
-    <h2>{content.Hed}</h2>
-    <p>{content.Dek}</p>
-  </section>
+  <!--  Read the docs: https://reuters-graphics.github.io/graphics-svelte-components/ -->
+  <Headline section="{content.Kicker}" hed="{content.Hed}" dek="{content.Dek}">
+    <span slot="byline"
+      >By <strong>Jane Doe</strong> & <strong>John Doe</strong></span
+    >
+    <span slot="dateline">Published Jan. 1, 2021</span>
+  </Headline>
 
+  <!-- Looping through you Gdoc blocks... -->
   {#each content.blocks as block}
     {#if block.Type === 'text'}
       <BodyText text="{block.Text}" />
