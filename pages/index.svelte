d80c77c542aa8575cf4a46761786a821fbe0938e
hobbes7878 <jon.r.mcclure@gmail.com>
2021-08-20 13:21:19 +0100

    bump components and fix default share image path

--- pages/index.svelte
+++ pages/index.svelte
@@ -16,7 +16,7 @@
   seoDescription="{content.SEODescription}"
   shareTitle="{content.ShareTitle}"
   shareDescription="{content.ShareDescription}"
-  shareImgPath="images/reuter-graphics.jpg"
+  shareImgPath="images/reuters-graphics.jpg"
   lang="en"
 />
 
