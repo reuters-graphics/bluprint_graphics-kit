3946b8c345a77e87044599c8763ae7d93f75eced
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2021-09-07 14:32:43 -0500

    cleaned chart component, fixed published dateline, commented out code for opening links in new tabs

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -1,48 +1,16 @@
 <script>
-  import Chart from '@reuters-graphics/parliament-chart';
+  import Chart from '@reuters-graphics/categorical-bar-chart';
   import { onMount } from 'svelte';
-  import debounce from 'lodash/debounce.js';
+  import debounce from 'lodash-es/debounce.js';
   export let title;
   export let chatter;
   export let source;
   export let note;
   export let size;
 
-  const colours = (p) => {
-    switch (p.id) {
-      case 'a':
-        return '#2c6075';
-      case 'b':
-        return '#7ebfc1';
-      case 'c':
-        return '#d1eeea';
-      case 'd':
-        return '#ffd430';
-      case 'e':
-        return '#cda200';
-      default:
-        return 'lightgrey';
-    }
-  };
-
   onMount(() => {
     const chart = new Chart();
-    chart
-      .selection('#parliament-chart')
-      .data([
-        { id: 'a', seats: 150 },
-        { id: 'b', seats: 125 },
-        { id: 'c', seats: 75 },
-        { id: 'd', seats: 50 },
-        { id: 'e', seats: 25 },
-        { id: 'f', seats: 10 },
-      ])
-      .props({
-        circle: {
-          fill: colours,
-        },
-      })
-      .draw();
+    chart.selection('#basic-barchart').draw();
 
     const resize = debounce(() => {
       chart.draw();
@@ -51,10 +19,10 @@
   });
 </script>
 
-<section class="graphic {size}">
+<section class="graphic borderless {size} borderless py-5">
   <h3>{title}</h3>
   <p>{chatter}</p>
-  <div id="parliament-chart"></div>
+  <div id="basic-barchart" class="barchart-container"></div>
   <aside>
     <p class="note mt-3">
       Note: {@html note}
@@ -64,4 +32,8 @@
 </section>
 
 <style lang="scss">
+  :global {
+    $barChart-container: '.barchart-container';
+    @import '~@reuters-graphics/categorical-bar-chart/src/scss/chart';
+  }
 </style>
--- src/lib/Page.svelte
+++ src/lib/Page.svelte
@@ -1,5 +1,7 @@
 <script>
   import content from '$locales/en/content.json';
+  import { apdate } from 'journalize';
+
   import {
     BodyText,
     Image,
@@ -7,20 +9,25 @@
     Headline,
   } from '@reuters-graphics/graphics-svelte-components';
   import Chart from './Chart.svelte';
-  import BasicBarchart from './BasicBarchart.svelte';
-  import StackedBarchar from './StackedBarchart.svelte';
+
+  /* Use these lines if you want to use d3 to make all links open in new tabs
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
   onMount(() => {
     d3.selectAll('a').attr('target', '_blank');
-  });
+  }); */
 </script>
 
 <article class="container-fluid">
   <!--  Read the docs: https://reuters-graphics.github.io/graphics-svelte-components/ -->
   <Headline section="{content.Kicker}" hed="{content.Hed}" dek="{content.Dek}">
     <span slot="byline">By {@html content.Byline} </span>
-    <span slot="dateline">Published Jan. 1, 2021</span>
+    <span slot="dateline">
+      Published <time datetime="{content.Published}">
+        {apdate(new Date(content.Published))}</time
+      >
+    </span>
+    <!-- Add new slot for Updated dateline? -->
   </Headline>
 
   <!-- Looping through you Gdoc blocks... -->
@@ -36,22 +43,6 @@
       />
     {:else if block.Type === 'graphic'}
       <Chart
-        title="{block.Title}"
-        chatter="{block.Chatter}"
-        source="{block.Source}"
-        note="{block.Note}"
-        size="{block.Size}"
-      />
-    {:else if block.Type === 'graphic-barchart'}
-      <BasicBarchart
-        title="{block.Title}"
-        chatter="{block.Chatter}"
-        source="{block.Source}"
-        note="{block.Note}"
-        size="{block.Size}"
-      />
-    {:else if block.Type === 'graphic-stacked-barchart'}
-      <StackedBarchar
         title="{block.Title}"
         chatter="{block.Chatter}"
         source="{block.Source}"
