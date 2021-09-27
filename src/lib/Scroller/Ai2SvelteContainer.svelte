d047bd7a7aa0d5eee25f839d94fef52d9379dcbe
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2021-09-27 17:51:24 -0500

    finished scroller ai2svelte component;

--- src/lib/Scroller/Ai2SvelteContainer.svelte
+++ src/lib/Scroller/Ai2SvelteContainer.svelte
@@ -3,38 +3,53 @@
   import Step1 from '../ai2html/step-1.svelte';
   import Step2 from '../ai2html/step-2.svelte';
   import Step3 from '../ai2html/step-3.svelte';
+  import Step4 from '../ai2html/step-4.svelte';
 
-  /* Don't touch the code below. */
+  /* Make sure the name you assign in the import lines above 
+  match what you put in allSteps.
+  */
+  const allSteps = [
+    { step: 1, ai2svelte: Step1 },
+    { step: 2, ai2svelte: Step2 },
+    { step: 3, ai2svelte: Step3 },
+    { step: 4, ai2svelte: Step4 },
+    // add more objects if you have more steps
+  ];
+
+  /* Don't touch the lines below. */
   export let index;
+  const lastStep = allSteps.length;
 
   /* 
   Check with Feilding:
   - Should we separate the Ai2SvelteContainer.svelte from the index.svelte?
-  - Is there a way to clean up the index logic to control opacity in this component? Current version assumes you always want to show the bototm layers. Should we make options for when you don't want the previous layers to show? If so, what to do about flashing?
-  
+- logic ok?  
 - Add an small empty section at end by default? Or make the last section taller by default?
   */
 </script>
 
-<h1>Step index: {index}</h1>
+<!-- You can delete the line below -->
+<h2>Step: {index}, total steps: {lastStep}</h2>
 
-<!-- <div
-  class="ai2svelte {index === 1 ? '' : index === 2 ? '' : 'hidden'}"
-  step="1"
-> -->
-<div class="ai2svelte" step="1">
-  <Step1 />
-</div>
-<!-- <div
-  class="ai2svelte {index === 2 ? '' : index === 3 ? '' : 'hidden'}"
-  step="2"
-> -->
-<div class="ai2svelte {index === 1 ? 'hidden' : ''}" step="2">
-  <Step2 />
-</div>
-<div class="ai2svelte {index === 3 ? '' : 'hidden'}" step="3">
-  <Step3 />
-</div>
+<!-- Generally, dont' touch the html codes below -->
+{#each allSteps as stepObj, i}
+  <!-- <div class="ai2svelte {index === i + 1 ? '' : 'hidden'}" step="{i + 1}"> -->
+  <div
+    class="ai2svelte-step 
+    {i + 1 == 1
+      ? 'visible'
+      : index === i + 1
+      ? 'visible'
+      : i + 1 < index
+      ? 'visible'
+      : index === lastStep
+      ? 'visible'
+      : 'hidden'}"
+    step="{i + 1}"
+  >
+    <svelte:component this="{stepObj.ai2svelte}" />
+  </div>
+{/each}
 
 <style lang="scss">
   .ai2svelte {
---
+++ src/lib/Scroller/OLD-Ai2SvelteContainer.svelte
@@ -0,0 +1,49 @@
+<script>
+  /* Add ai2svelte files here. */
+  import Step1 from '../ai2html/step-1.svelte';
+  import Step2 from '../ai2html/step-2.svelte';
+  import Step3 from '../ai2html/step-3.svelte';
+
+  /* Don't touch the code below. */
+  export let index;
+
+  /* 
+  Check with Feilding:
+  - Should we separate the Ai2SvelteContainer.svelte from the index.svelte?
+  - Is there a way to clean up the index logic to control opacity in this component? Current version assumes you always want to show the bototm layers. Should we make options for when you don't want the previous layers to show? If so, what to do about flashing?
+  
+- Add an small empty section at end by default? Or make the last section taller by default?
+  */
+</script>
+
+<h1>Step index: {index}</h1>
+
+<!-- <div
+  class="ai2svelte {index === 1 ? '' : index === 2 ? '' : 'hidden'}"
+  step="1"
+> -->
+<div class="ai2svelte" step="1">
+  <Step1 />
+</div>
+<!-- <div
+  class="ai2svelte {index === 2 ? '' : index === 3 ? '' : 'hidden'}"
+  step="2"
+> -->
+<div class="ai2svelte {index === 1 ? 'hidden' : ''}" step="2">
+  <Step2 />
+</div>
+<div class="ai2svelte {index === 3 ? '' : 'hidden'}" step="3">
+  <Step3 />
+</div>
+
+<style lang="scss">
+  .ai2svelte {
+    width: 100%;
+    position: absolute;
+    transition: 0.5s opacity ease;
+
+    &.hidden {
+      opacity: 0;
+    }
+  }
+</style>
