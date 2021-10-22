e41dd7fb7ffc15b3585c277b573eeae00bf13370
hobbes7878 <jon.r.mcclure@gmail.com>
2021-10-22 11:18:25 +0100

    example of pulling in font styles

--- src/lib/components/ExampleComponent/index.svelte
+++ src/lib/components/ExampleComponent/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="graphic">
-  <h5>The count is {count}</h5>
+  <h5>The count is <span>{count}</span></h5>
   <button
     on:click="{() => {
       count += 1;
@@ -12,9 +12,14 @@
 </section>
 
 <style lang="scss">
+  @import '~@reuters-graphics/style-main/scss/fonts/mixins';
+
   section.graphic {
     h5 {
       color: blue;
+      span {
+        @include font-monospace;
+      }
     }
   }
 </style>
