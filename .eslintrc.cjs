eaeb6930acc908dd0a8c6ccc201c62a9eb684a79
hobbes7878 <jon.r.mcclure@gmail.com>
2021-10-22 10:58:38 +0100

    align eslint and prettier on comma dangle

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
       {
         arrays: 'always-multiline',
         objects: 'always-multiline',
-        imports: 'never',
+        imports: 'always-multiline',
         exports: 'never',
         functions: 'never',
       },
