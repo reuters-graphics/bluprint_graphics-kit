5f89241f6a6fe43e6e8ee522e0d48a43c6732672
hobbes7878 <jon.r.mcclure@gmail.com>
2022-02-06 20:45:11 +0000

    update virtual pages plugin so you can dynamically add and remove pages w/out reloading the server

--- bin/svelte-kit/plugins/svelte-kit-pages/index.cjs
+++ bin/svelte-kit/plugins/svelte-kit-pages/index.cjs
@@ -1,12 +1,15 @@
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
+const { normalizePath } = require('vite');
 
 const getPkgRoot = () => {
   const PKG_PATH = path.join(process.cwd(), 'package.json');
   if (!fs.existsSync(PKG_PATH)) throw new Error('Unable to find package.json in your current working directory. Are you running from the root of your project?');
   return process.cwd();
 };
+
+let FOUND_PAGES = [];
 
 // Gets paths to pages in SvelteKit
 const getPagePaths = (base = '/', pagesDir = 'pages') => {
@@ -17,22 +20,35 @@ const getPagePaths = (base = '/', pagesDir = 'pages') => {
     const pagePath = path.join(base, embed.replace('.svelte', ''));
     return /index$/.test(pagePath) ? pagePath.replace(/index$/, '') : pagePath;
   });
+  FOUND_PAGES = pages.map(embed => path.join(PAGES_DIR, embed));
   return `export default ['${pagePaths.join('\', \'')}'];`;
 };
 
 module.exports = function svelteKitPagesPlugin({ base = '/', pages = 'pages' } = {}) {
-  const virtualModuleId = '@svelte-kit-pages';
+  const VIRTUAL_MODULE_ID = '@svelte-kit-pages';
+  const PAGES_DIR = path.join(process.cwd(), pages);
 
   return {
     name: 'svelte-kit-pages-plugin',
     resolveId(id) {
-      if (id === virtualModuleId) {
-        return virtualModuleId;
+      if (id === VIRTUAL_MODULE_ID) {
+        return VIRTUAL_MODULE_ID;
       }
     },
     load(id) {
-      if (id === virtualModuleId) {
+      if (id === VIRTUAL_MODULE_ID) {
         return getPagePaths(base, pages);
+      }
+    },
+    configureServer(server) {
+      server.watcher.add(normalizePath(PAGES_DIR));
+    },
+    handleHotUpdate(hmr) {
+      const { server, file } = hmr;
+      // If a page is added and not already in pages we've found, reload the plugin...
+      if (file.includes(PAGES_DIR) && !FOUND_PAGES.includes(file)) {
+        const plugin = server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID);
+        server.moduleGraph.invalidateModule(plugin);
       }
     },
   };
