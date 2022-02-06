081fd001e63e119f343b9deea520149983a49a50
hobbes7878 <jon.r.mcclure@gmail.com>
2022-02-06 21:23:45 +0000

    handle page deletes too

--- bin/svelte-kit/plugins/svelte-kit-pages/index.cjs
+++ bin/svelte-kit/plugins/svelte-kit-pages/index.cjs
@@ -9,46 +9,55 @@ const getPkgRoot = () => {
   return process.cwd();
 };
 
-let FOUND_PAGES = [];
-
-// Gets paths to pages in SvelteKit
-const getPagePaths = (base = '/', pagesDir = 'pages') => {
-  const ROOT = getPkgRoot();
-  const PAGES_DIR = path.resolve(ROOT, pagesDir);
-  const pages = glob.sync('**/*.svelte', { cwd: PAGES_DIR });
-  const pagePaths = pages.map((embed) => {
-    const pagePath = path.join(base, embed.replace('.svelte', ''));
-    return /index$/.test(pagePath) ? pagePath.replace(/index$/, '') : pagePath;
-  });
-  FOUND_PAGES = pages.map(embed => path.join(PAGES_DIR, embed));
-  return `export default ['${pagePaths.join('\', \'')}'];`;
-};
-
 module.exports = function svelteKitPagesPlugin({ base = '/', pages = 'pages' } = {}) {
   const VIRTUAL_MODULE_ID = '@svelte-kit-pages';
-  const PAGES_DIR = path.join(process.cwd(), pages);
+  const PAGES_DIR = path.join(getPkgRoot(), pages);
+
+  let FOUND_PAGES = [];
+
+  // Gets paths to pages
+  const getPagePaths = () => {
+    const pages = glob.sync('**/*.svelte', { cwd: PAGES_DIR });
+    // Reset FOUND_PAGES
+    FOUND_PAGES = pages.map(embed => normalizePath(path.join(PAGES_DIR, embed)));
+    // Remove Svelte-specific extensions
+    const pagePaths = pages.map((embed) => {
+      const pagePath = path.join(base, embed.replace('.svelte', ''));
+      return /index$/.test(pagePath) ? pagePath.replace(/index$/, '') : pagePath;
+    });
+    // Return as virtual module
+    return `export default ['${pagePaths.join('\', \'')}'];`;
+  };
+
+  const reloadModule = (server) => {
+    const plugin = server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID);
+    server.moduleGraph.invalidateModule(plugin);
+  };
 
   return {
     name: 'svelte-kit-pages-plugin',
+
     resolveId(id) {
-      if (id === VIRTUAL_MODULE_ID) {
-        return VIRTUAL_MODULE_ID;
-      }
+      if (id === VIRTUAL_MODULE_ID) return VIRTUAL_MODULE_ID;
     },
+
     load(id) {
-      if (id === VIRTUAL_MODULE_ID) {
-        return getPagePaths(base, pages);
-      }
+      if (id === VIRTUAL_MODULE_ID) return getPagePaths();
     },
+
     configureServer(server) {
       server.watcher.add(normalizePath(PAGES_DIR));
+      server.watcher.on('unlink', (f) => {
+        const file = normalizePath(f);
+        if (FOUND_PAGES.includes(file)) reloadModule(server);
+      });
     },
-    handleHotUpdate(hmr) {
-      const { server, file } = hmr;
+
+    handleHotUpdate({ server, file }) {
       // If a page is added and not already in pages we've found, reload the plugin...
       if (file.includes(PAGES_DIR) && !FOUND_PAGES.includes(file)) {
-        const plugin = server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID);
-        server.moduleGraph.invalidateModule(plugin);
+        reloadModule(server);
+        server.watcher.add(normalizePath(file));
       }
     },
   };
