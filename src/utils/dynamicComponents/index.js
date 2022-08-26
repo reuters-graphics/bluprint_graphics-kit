a4a3a4d3f255fa7f364f90ac96a298e9c0d674f6
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-26 21:03:26 +0100

    cleanup utils

--- src/utils/dynamicComponents/index.js
+++ src/utils/dynamicComponents/index.js
@@ -2,15 +2,3 @@ export const fetchComponent = async(componentName) => {
   return (await import(`./../../lib/ai2svelte/${componentName}.svelte`))
     .default;
 };
-
-export const makeScrollerSteps = async(steps) => {
-  const scrollerSteps = [];
-  for (const step of steps) {
-    const background = await fetchComponent(step.Background);
-    scrollerSteps.push({
-      background,
-      foreground: step.Foreground,
-    });
-  }
-  return scrollerSteps;
-};
--- src/utils/statics/index.js
+++
@@ -1,4 +0,0 @@
-import { assets } from '$app/paths';
-import urljoin from 'proper-url-join';
-
-export const getPath = (staticAssetPath = '') => urljoin(assets, staticAssetPath);
