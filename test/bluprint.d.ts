f078d4b1a416e8dde3d4c2bfe0d8baaf531fbc6f
hobbes7878 <jon.r.mcclure@gmail.com>
2024-11-26 15:28:33 +0000

    remove bluprint test and update deps

--- test/bluprint.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@reuters-graphics/bluprint' {
-  interface Bluprint {
-    start: (projectName: string) => Promise<void>;
-  }
-
-  const bluprint: Bluprint;
-  export default bluprint;
-}
